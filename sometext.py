38c978f8456a1064a94df28cb1eedd0e8cff72d8
Wesley Darling <wesley_darling@berkeley.edu>
2022-02-14 20:33:52 +0000

    created sometext

---
+++ sometext.py
@@ -0,0 +1,30 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+### We make changes
+
+# %%
+print("In an .ipynb file")
+
+
+# %%
+print("that way"*4)
+
+# %% [markdown]
+# we can save it as a ***python file***
+
+# %% [markdown]
+# ## And avoid merge conflicts? ##
